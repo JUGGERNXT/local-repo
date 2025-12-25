af8068e035ba3a4168c012fb091ef2657a35cfd7
JUGGERNXT <19aniket.chaudhary@gmail.com>
2025-12-25 18:01:13 +0530

    feature added

--- b.java
+++ b.java
@@ -4,5 +4,6 @@ public class b {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
         System.out.println(arr);
+        System.out.println("This is a new feature");
     }
 }
