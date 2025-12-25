1de18ab04c99150cbe1da9b5a0d364df538370c4
JUGGERNXT <19aniket.chaudhary@gmail.com>
2025-12-25 18:26:53 +0530

    changing

--- b.java
+++ b.java
@@ -5,5 +5,6 @@ public class b {
         int arr[] = {1,2,3,4,5};
         System.out.println(arr);
         System.out.println("This is a new feature");
+        System.out.println("changes");
     }
 }
